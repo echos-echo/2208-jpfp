aab8a02cc2243749bee6031cbdc776bb4f1e74e0
Eve Cho <e.young.echo@gmail.com>
2022-08-29 14:08:13 -0400

    changing store index to toolkit configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import {createStore, applyMiddleware, combineReducers} from "redux";
 import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
 
+// function configureStore() {
+//     // return createStore(########, applyMiddleware(thunk));
+// }
 
-function configureStore() {
-    // return createStore(########, applyMiddleware(thunk));
-}
+// export default configureStore;
 
-export default configureStore;
+export const store = configureStore({
+    reducer: {
+        // some reducers go here
+    }
+})
