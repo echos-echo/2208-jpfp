2d02ccd6f154ad3b7f64502b3aeaac406967bcdb
Eve Cho <e.young.echo@gmail.com>
2022-08-29 13:29:16 -0400

    students router created

---
+++ server/api/students.js
@@ -0,0 +1,12 @@
+const studentsRouter = require('express').Router();
+const Student = require('../db/student');
+
+studentsRouter.get('/', async (req, res, next) => {
+    try {
+        res.send(await Student.findAll())
+    } catch(err) {
+        next(err);
+    }
+})
+
+module.exports = studentsRouter;
