9264e38171451deb34572658f432b35d21b8e8cb
Eve Cho <e.young.echo@gmail.com>
2022-08-29 14:13:14 -0400

    studentsReducer complete

---
+++ src/store/studentsReducer.js
@@ -0,0 +1,28 @@
+import Axios from "axios";
+import { useDispatch } from "react-redux";
+
+const _getStudents = 'GET_STUDENTS';
+
+const getStudents = data => {
+    return {
+        type: _getStudents,
+        students: data
+    }
+}
+
+export const getAllStudentsThunk = () => {
+    return async dispatch => {
+        await Axios.get('/api/students')
+            .then(res => dispatch(getStudents(res.data)))
+            .catch(err => console.error(err));
+    }
+}
+
+export default studentsReducer = (state = {}, action) => {
+    switch (action.type) {
+        case _getStudents:
+            return { ...state, students: action.students};
+        default:
+            return state;
+    }
+}
